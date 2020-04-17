8f4410fa539f86499a40ff1ec0510a4219e1c665
OndrejGrygar <61903826+OndrejGrygar@users.noreply.github.com>
2020-04-17 18:48:49 +0200

    Update testing_web_respons.py

--- testing_web_respons.py
+++ testing_web_respons.py
@@ -1,17 +1,26 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import time
 
 with open("websites_dict.json") as file:
     content = json.load(file)
 print(content)
 
-for key,value in content.items():
-    for i in value:
-        for key1,value1 in i.items():
-            response = requests.get(value1)
-            print("Response of website", value1, response.ok)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            tag = soup.find("title")
-            print(tag)
+while True:
+    for key,value in content.items():
+        for i in value:
+            for key1, value1 in i.items():
+                try:
+                    response = requests.get(value1)
+                    print("Response of website", value1, response.ok)
+
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    tag = soup.find("title")
+                    print("Tag Title of website:", tag)
+                    time.sleep(4)
+                except Exception:
+                    print(value1, "wrong Url is provided or server is down")
+                    continue
+
 
